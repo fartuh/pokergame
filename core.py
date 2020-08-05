eedf16eba6b82aa335da3855f71cd7e46c32eb77
Winzmcman <nikithobbi@gmail.com>
2020-08-05 17:22:49 +0300

    dev1.3

--- core.py
+++ core.py
@@ -21,21 +21,47 @@ class Game():
                 picked = random.choice(self.cards)
                 picked_card = picked[1]
 
-                self.robotcards.append(picked_card) 
+                self.robotcards.append(picked) 
                 self.cards.remove(picked)
             else:
                 picked = random.choice(self.cards)
                 picked_card = picked[1]
 
-                self.playercards.append(picked_card) 
+                self.playercards.append(picked) 
                 self.cards.remove(picked)
 
         print("Твои карты")
-        print(self.playercards)
+        for card in self.playercards:
+            print(card[1])
+
         print("Карты твоего оппонента")
-        print(self.robotcards)
+        for card in self.robotcards:
+            print(card[1])
+
+        self.findWinner()
 
 
     def getCards(self):
         self.dbcontroller = dbcontroller.DBController('db/cards.sqlite3')
         self.cards = self.dbcontroller.getAllCards()         
+
+    def findWinner(self):
+        p_s_arr = []
+        r_s_arr = []
+        
+        for arr in self.playercards:
+            p_s_arr.append(arr[2]) 
+        
+        p_s = max(p_s_arr)
+
+        for arr_r in self.robotcards:
+            r_s_arr.append(arr_r[2]) 
+            
+        r_s = max(r_s_arr)
+
+        if p_s > r_s:
+            print('Ты победил')
+        elif r_s > p_s:
+            print('Ты проиграл')
+        else:
+            print('Ничья')
