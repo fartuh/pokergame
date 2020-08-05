14df27d82bda9b41a3ee1b26ca2eab56dcd12765
Winzmcman <nikithobbi@gmail.com>
2020-08-05 19:45:58 +0300

    dev1.4

--- core.py
+++ core.py
@@ -46,6 +46,15 @@ class Game():
         self.cards = self.dbcontroller.getAllCards()         
 
     def findWinner(self):
+        winner = self.highest()
+        pairs = self.pair()
+        if pairs != 0:
+            winner = pairs
+
+        print(winner)
+        
+
+    def highest(self):
         p_s_arr = []
         r_s_arr = []
         
@@ -54,14 +63,49 @@ class Game():
         
         p_s = max(p_s_arr)
 
-        for arr_r in self.robotcards:
-            r_s_arr.append(arr_r[2]) 
+        for arr in self.robotcards:
+            r_s_arr.append(arr[2]) 
             
         r_s = max(r_s_arr)
 
         if p_s > r_s:
-            print('Ты победил')
+            return 'player'
         elif r_s > p_s:
-            print('Ты проиграл')
+            return 'robot'
         else:
-            print('Ничья')
+            return 'both'
+
+    def pair(self):
+        player_pair = 0
+        robot_pair = 0
+
+        for card in self.playercards:
+            for i in range(5):
+                if card[2] == self.playercards[i][2] and card[1] != self.playercards[i][1]:
+                    if card[2] > player_pair:
+                        player_pair = card[2]
+                        break
+
+        for card in self.robotcards:
+            if robot_pair != 0:
+                break
+            for i in range(5):
+                if card[2] == self.robotcards[i][2] and card[1] != self.robotcards[i][1]:
+                    if card[2] > player_pair:
+                        robot_pair = card[2]
+                        break
+
+        
+        if player_pair > robot_pair and player_pair > 0:
+            return 'player'
+        if robot_pair > player_pair and robot_pair > 0:
+            return 'robot'
+        if robot_pair == player_pair and player_pair == 0:
+            return 0
+        else:
+            return 'both'
+
+
+
+
+
